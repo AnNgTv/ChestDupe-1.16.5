ec419ea6dd7b023adfb2ebf453cae0007c77fcd6
anmcvn2k9-png <anmcvn2k9@gmail.com>
2026-05-12 13:19:31 +0700

    Initial commit

---
+++ src/main/java/me/saturn/forceop/Main.java
@@ -0,0 +1,10 @@
+package me.saturn.forceop;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Force OP Mod (1.16.5) initialized");
+	}
+}
